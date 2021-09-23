73f190b1535362263432e9edb313f1ac0de92bc9
manikantareddyj <82630925+manikantareddyj@users.noreply.github.com>
2021-09-23 10:30:44 +0530

    Update newserver.tf

--- newserver.tf
+++ newserver.tf
@@ -1,10 +1,50 @@
 provider "aws" {
-  profile = "default"
-  region  = "ap-south-1"
+ region = "ap-south-1"
 }
 
-resource "aws_instance" "aws-server-1" {
-  ami           = "ami-0c1a7f89451184c8b"
-  instance_type = "t2.micro"
-  key_name =  "all"
+    resource "aws_instance" "ec" {
+        ami = "ami-06a0b4e3b7eb7a300"
+        instance_type = "t2.micro"
+ security_groups = [aws_security_group.sa.name]
+        key_name = "sample"
+           tags = {
+           Name = "demo"
+                  }
+     }
+
+
+resource "aws_default_vpc" "main" {
+ 
+
+  tags = {
+    Name = "main"
+  }
+}
+
+
+resource "aws_security_group" "sa" {
+  name        = "sa"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_default_vpc.main.id
+
+  ingress = [
+
+    {
+      description      = "ssh-port"
+      from_port        = 22
+      to_port          = 22
+      protocol         = "tcp"
+      cidr_blocks      = [aws_default_vpc.main.cidr_block]
+      ipv6_cidr_blocks = []
+ prefix_list_ids =[]
+ self   = false
+ security_groups =[]
+    }
+  ]
+
+ 
+
+  tags = {
+    Name = "sa"
+  }
 }
