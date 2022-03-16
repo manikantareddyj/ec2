2d2aba373a6d6c50eacd9e86509fa33a02533b90
manikantareddyj <82630925+manikantareddyj@users.noreply.github.com>
2022-03-16 17:45:17 +0530

    Update newserver.tf

--- newserver.tf
+++ newserver.tf
@@ -6,7 +6,7 @@ provider "aws" {
         ami = "ami-06a0b4e3b7eb7a300"
         instance_type = "t2.micro"
  security_groups = [aws_security_group.sa.name]
-        key_name = "sample"
+        key_name = "test"
            tags = {
            Name = "demo"
                   }
